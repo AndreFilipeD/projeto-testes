cdcdc5ffad047f45b0a4039c9b952a78b8491aab
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-11-16 19:09:52 -0300

    Atualização de responsividade

--- imperio/script/script.js
+++ imperio/script/script.js
@@ -1,13 +1,13 @@
 let slide = 1;
-var widthis
-var mslider = window.document.getElementById("mainSlider")
-window.document.getElementById("sliderbtn1").checked = true
+var widthis;
+var mslider = window.document.getElementById("mainSlider");
+window.document.getElementById("sliderbtn1").checked = true;
 
 setInterval(function(){nextImage()},5000)// Atualização automatica do Slider.
 function clickslide(is){slide = is}//Atualização manual do Slider
 function nextImage(){
     if(slide>3){slide=1}else{slide++}//funçao automatica do Slider
-    window.document.getElementById("sliderbtn"+slide).checked = true
+    window.document.getElementById("sliderbtn"+slide).checked = true;
 }
 
 function clickthrow(){
