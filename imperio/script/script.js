ec1f85f21ebba0b990c84036146bf78945a4b314
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-11-21 14:29:48 -0300

    Adição de menu funcional a esquerda

--- imperio/script/script.js
+++ imperio/script/script.js
@@ -3,16 +3,32 @@ var widthis;
 var mslider = window.document.getElementById("mainSlider");
 window.document.getElementById("sliderbtn1").checked = true;
 
-setInterval(function(){nextImage()},5000)// Atualização automatica do Slider.
-function clickslide(is){slide = is}//Atualização manual do Slider
+var sliderloop = setInterval(function(){nextImage()}, 6000)// Atualização automatica do Slider.
+
+function clickslide(is){//Atualização manual do Slider
+  slide = is
+  clearInterval(sliderloop);/*Reinicializador de tempo após clicar manualmente*/
+  sliderloop = setInterval(function(){nextImage()}, 6000);
+}
+
 function nextImage(){
     if(slide>3){slide=1}else{slide++}//funçao automatica do Slider
     window.document.getElementById("sliderbtn"+slide).checked = true;
 }
 
-function clickthrow(){
-    //window.document.getElementById("mainSlider").src='slides/slidemob.html'
-    document.getElementById('elementID').click();
+function clickthrow(valbutton){
+  if(valbutton===0){
+    window.document.getElementById("htmlSet").style.overflowY = "scroll";
+    window.document.querySelector(".shadowBox").style="display: none;"
+    window.document.querySelector(".left_menuBar").style="animation-name: menuClose;"
+    /*"display: none;"*/
+  }else if(valbutton===1){
+    window.document.getElementById("htmlSet").style.overflowY = "hidden";
+    window.document.querySelector(".shadowBox").style="display: absolute;"
+    window.document.querySelector(".left_menuBar").style="animation-name: menuOpen;"/*"display: absolute;margin-left: 100px;"*/
+  }
+    /*window.document.getElementById("mainSlider").src='slides/slidemob.html'
+    document.getElementById('elementID').click();*/
 }
 
 function widthResizer(){
