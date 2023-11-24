2a5f1ce930e63316a8d7c4cbfe650d9fb3720b8c
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-11-24 09:18:19 -0300

    Primeiro protótipo funcional do campo estelar

--- stars/script.js
+++ stars/script.js
@@ -10,25 +10,29 @@ var canvElement = window.document.getElementById("canvEl")
 function canvaUpdate(){
     alt = window.innerHeight
     larg = window.innerWidth
-    ("starfield")
     canvElement.innerHTML='<canvas id="starfield" width="'+larg+'" height="'+alt+'"></canvas>'
     canvas = window.document.getElementById("starfield")
     ctx = canvas.getContext("2d")
-    window.alert('Atualizado')
+    //window.alert('Atualizado')
 }
 //criar o canvas via javascript!
 //window.alert("ALTURA:"+alt+"LARGURA"+larg)
+function refreshField(){
+    for(var i=0; i<(alt*larg)/600; i++){
 
-for(var i=0; i<30000; i++){
+        if(i===0){canvaUpdate()}
 
-    if(i===0){canvaUpdate()}
-
-    x = Math.floor(Math.random() * larg)
-    y = Math.floor(Math.random() * alt)
-    ctx.beginPath()
-    ctx.moveTo(x, y)
-    ctx.lineTo(x+1, y+1)
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#ffffff';
-    ctx.stroke();
+        x = Math.floor(Math.random() * larg)
+        y = Math.floor(Math.random() * alt)
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+        ctx.lineTo(x+1, y+1)
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#ffffff';
+        ctx.stroke();
+    }
 }
+refreshField()
+const loop = setInterval((event) => {
+    refreshField()
+},1000)
