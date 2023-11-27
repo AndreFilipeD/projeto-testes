765561974cf6d4c3ee0b3bed6fa6c02ca0108488
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-11-27 11:33:23 -0300

    Atualização de controle das estrelas

--- stars/amazing starfield/script.js
+++ stars/amazing starfield/script.js
@@ -1,16 +1,19 @@
 var stars = []//Espaço de memória das estrelas
 
-var outfieldMove = 1000// (100) muito | (1000) pouco
+var outfieldMove = 1000 // (100) muito | (1000) pouco
 //O quando as estrelas fogem do campo de visão baseado nos angulos de saida
-var maxSize = 35//(10) ridiculo | grande(50)
+var maxSize = 35 //(10) pequeno | (50) grande
 //Tamanho máximo da estrela até que ela suma da tela
+var grownFrame = 0.1 //(0.1) pouco | (1) muito
+//O quando a estrela cresce por frame
+var frameUpdateTime = 100 //(16) Rapido | (200) Lentidão
+//Delay de atualização de quadros
+
 var alt = window.innerHeight //Captura de Altura inicial da tela
 var larg = window.innerWidth //Captura de Largura inicial da tela
 var canvas = window.document.getElementById("starfield")//Coletando Canvas
 var ctx = canvas.getContext("2d")//Coletando contexto 2D do Canvas
 var canvElement = window.document.getElementById("canvEl")//Coletando Body para atualizar canvas via HTML
-
-
 
 function canvaUpdate(){//Atualiza o canvas para a area de exibição do navegador
     alt = window.innerHeight//Atualização altura da tela
@@ -27,13 +30,10 @@ function forgeStar(){//Forjando Estrelas
         stars.push({"x":x,"y":y,"mx":(x - (larg/2))/outfieldMove,"my":(y - (larg/2))/outfieldMove, "s":Math.random()*maxSize})
     }
 }
-
-forgeStar()//Forjando Estrelas
-
+forgeStar()//Forjando as Estrelas
 
 function refreshField(){//Atualização das estrelas
-    
-    canvaUpdate()
+        canvaUpdate()
         for(let i=0;i<stars.length;i++){
             /*
             x = Math.floor(Math.random() * larg)
@@ -45,9 +45,9 @@ function refreshField(){//Atualização das estrelas
                 stars[i].mx=(stars[i].x - (larg/2))/outfieldMove
                 stars[i].my=(stars[i].y - (alt/2))/outfieldMove
            }else{
-                stars[i].s+=0.5
-                stars[i].x+=stars[i].mx * (stars[i].s)*(stars[i].s * 6)
-                stars[i].y+=stars[i].my * (stars[i].s)*(stars[i].s * 6)
+                stars[i].s+=0.1
+                stars[i].x+=stars[i].mx * (stars[i].s)*(stars[i].s)
+                stars[i].y+=stars[i].my * (stars[i].s)*(stars[i].s)
 
                 ctx.beginPath();//Grafico das estrelas
                 ctx.arc(stars[i].x, stars[i].y, stars[i].s, 0, 2 * Math.PI);
@@ -56,27 +56,9 @@ function refreshField(){//Atualização das estrelas
            }
             
         }
-        
-            
-        /*
-        ctx.beginPath();
-        ctx.arc(100, 100, 5, 0, Math.PI*2, true); 
-        ctx.closePath();
-        ctx.strokeStyle = '#ffffff';
-        ctx.fill();
-        */
-
-        /*
-        ctx.beginPath()
-        ctx.moveTo(x, y)
-        ctx.lineTo(x+1, y+1)
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = '#ffffff';
-        ctx.stroke();
-        */
 }
 
 refreshField()
 const loop = setInterval((event) => {//Loop de atualização
     refreshField()
-},100)
+},frameUpdateTime)
