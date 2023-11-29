dc9b26a5884e589ef1751cb83c650f05133d31d7
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-11-29 11:26:56 -0300

    Documentação de estilo principal

--- imperio/script/produtos.js
+++ imperio/script/produtos.js
@@ -1,57 +1,52 @@
-var mostruario = window.document.getElementById('showProduct').style="display:none;"
+//window.document.getElementById('showProduct').style="display:none;"
+
 var sliderPage = window.document.getElementById('mainSlider')
 var filtro = "none";
 var cont = 0
 var produtosLoja = [
     //---------------------------------------------
-    {indice:'1',
+    {indice:0, alt:'Camiseta',
     img:'produtos/roupas_Teste/blusa_exemplo.png',
-    alt:'Camiseta',
     nome:'Blusa de exemplo',
     info:'Blusa branca padrão feita de algodão',
     valorAntigo:'',
     valorAtual:'R$ 39,99',
     categoria:['Camisa']}
     ,//---------------------------------------------
-    {indice:'2',
+    {indice:0, alt:'Conjunto',
     img:'produtos/roupas_Teste/conjunto_casal_exemplo.png',
-    alt:'Conjunto',
     nome:'Super conjunto social de casal para festas de exemplo',
     info:'Conjunto social de luxo para eventos de alto nivel',
     valorAntigo:'R$ 199,90',
     valorAtual:'R$ 129,90',
     categoria:['Camisa','Vestido','Calça']}
     ,//---------------------------------------------
-    {indice:'3',
+    {indice:0, alt:'Conjunto',
     img:'produtos/roupas_Teste/conjunto_feminino_exemplo.png',
-    alt:'Conjunto',
     nome:'Conjunto feminino de exemplo',
     info:'Conjunto de peças feminina para o dia a dia',
     valorAntigo:'R$ 89,90',
     valorAtual:'R$ 74,49',
     categoria:['Camisa','Vestido']}
     ,//---------------------------------------------
-    {indice:'4',
+    {indice:0, alt:'Conjunto',
     img:'produtos/roupas_Teste/conjunto_masculino_exemplo.png',
-    alt:'Conjunto',
     nome:'Conjunto masculino de exemplo',
     info:'Conjunto masculino leve e respirável para a prática de esporte',
     valorAntigo:'R$ 99,90',
     valorAtual:'R$ 64,90',
     categoria:['Camisa','Calça']}
     ,//---------------------------------------------
-    {indice:'5',
+    {indice:0, alt:'Conjunto',
     img:'produtos/roupas_Teste/conjunto_unisex_exemplo.png',
-    alt:'Conjunto',
     nome:'Conjunto unisex de exemplo',
     info:'Camiseta branca e calça jeans unisex padrão para uso geral',
     valorAntigo:'R$ 74,99',
     valorAtual:'R$ 69,90',
     categoria:['Camisa','Calça']}
     ,//---------------------------------------------
-    {indice:'6',
+    {indice:0, alt:'Vestido',
     img:'produtos/roupas_Teste/vestido_exemplo.png',
-    alt:'Vestido',
     nome:'Vestido social de exemplo',
     info:'Vestido social embedado em joias para eventos sociais',
     valorAntigo:'',
@@ -60,17 +55,22 @@ var produtosLoja = [
 
 ]//||------||------|| FIM DOS PRODUTOS DA LOJA ||------||------||
 
-function cleanProduct(){
+function startIndice(){// ATRIBUI O INDICE DE CADA ITEM AUTOMATICAMENTE
+    for(c in produtosLoja){
+        produtosLoja[c].indice=Number(c)+1}
+}startIndice()
+
+function cleanProduct(){// REMOVE TODOS OS PRODUTOS DO SITE PARA ATUALIZAÇÃO
     window.document.querySelector(".shopItens").innerHTML="";cont=0;
 }
 
-function loadProduct(filtroSelecionado){
-    filtro=filtroSelecionado
+function loadProduct(filtroSelecionado){// CARREGA E FILTRA PRODUTOS SE NECESSÁRIO
+    filtro=filtroSelecionado // LINHA DISPENSAVEL?
     cleanProduct();
     for(c in produtosLoja){
         if(filtro==="none"){
             cont++
-            window.document.querySelector(".shopItens").innerHTML+=`<div class='shopProduct' onclick="showProduct(${produtosLoja.indexOf(this)})"><img src="${produtosLoja[c].img}" alt="${produtosLoja[c].alt}"><div class='descProduto'><h1 class='nomeProduto'>${produtosLoja[c].nome}</h1><p class='antigoProduto'>${produtosLoja[c].valorAntigo}</p><p class='valorProduto'>${produtosLoja[c].valorAtual}</p></div></div>`
+            window.document.querySelector(".shopItens").innerHTML+=`<div class='shopProduct' onclick="showProduct(${produtosLoja[c].indice})"><img src="${produtosLoja[c].img}" alt="${produtosLoja[c].alt}"><div class='descProduto'><h1 class='nomeProduto'>${produtosLoja[c].nome}</h1><p class='antigoProduto'>${produtosLoja[c].valorAntigo}</p><p class='valorProduto'>${produtosLoja[c].valorAtual}</p></div></div>`
 
             window.document.querySelector("#msgIntro").innerHTML="Seja bem vindo!"
         }else if(produtosLoja[c].categoria.indexOf(filtro)!=-1){
@@ -88,14 +88,11 @@ function loadProduct(filtroSelecionado){
     }*/
 }loadProduct("none");
 
-function showProduct(showIs){
+function showProduct(showIs){// APRESENTA PRODUTO SELECIONADO NO MOSTRUÁRIO
     if(showIs === 0){
-        // HIDE CONTENT
+        window.document.getElementById('showProduct').style="display:none;"
     }else{
-        for(c in produtosLoja){
-            if(c===showIs){
-                // SHOW CONTENT
-            }
-        }
+        window.document.getElementById('showProduct').style="display:flex;"
+        window.document.getElementById('showProduct').innerHTML+=`${showIs}`
     }
 }
