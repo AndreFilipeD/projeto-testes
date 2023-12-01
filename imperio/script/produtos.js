7e4844e67cd62548c1c284a2bed245ae20aac078
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-12-01 10:42:32 -0300

    Atualização do mostruário

--- imperio/script/produtos.js
+++ imperio/script/produtos.js
@@ -9,7 +9,7 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/blusa_exemplo.png',
     nome:'Blusa de exemplo',
     info:'Blusa branca padrão feita de algodão',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'198g',tamanho:'M',material:'algodão',
     valorAntigo:'',
     valorAtual:'R$ 39,99',
     categoria:['Camisa']}
@@ -18,7 +18,7 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/conjunto_casal_exemplo.png',
     nome:'Super conjunto social de casal para festas de exemplo',
     info:'Conjunto social de luxo para eventos de alto nivel',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'753g',tamanho:'M e P',material:'algodão, poliester e nanofibras',
     valorAntigo:'R$ 199,90',
     valorAtual:'R$ 129,90',
     categoria:['Camisa','Vestido','Calça']}
@@ -27,7 +27,7 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/conjunto_feminino_exemplo.png',
     nome:'Conjunto feminino de exemplo',
     info:'Conjunto de peças feminina para o dia a dia',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'378g',tamanho:'M',material:'algodão e poliester',
     valorAntigo:'R$ 89,90',
     valorAtual:'R$ 74,49',
     categoria:['Camisa','Vestido']}
@@ -36,7 +36,7 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/conjunto_masculino_exemplo.png',
     nome:'Conjunto masculino de exemplo',
     info:'Conjunto masculino leve e respirável para a prática de esporte',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'328g',tamanho:'M',material:'Poliester',
     valorAntigo:'R$ 99,90',
     valorAtual:'R$ 64,90',
     categoria:['Camisa','Calça']}
@@ -45,7 +45,7 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/conjunto_unisex_exemplo.png',
     nome:'Conjunto unisex de exemplo',
     info:'Camiseta branca e calça jeans unisex padrão para uso geral',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'521g',tamanho:'M',material:'algodão e Jeans',
     valorAntigo:'R$ 74,99',
     valorAtual:'R$ 69,90',
     categoria:['Camisa','Calça']}
@@ -54,16 +54,16 @@ var produtosLoja = [
     img:'produtos/roupas_Teste/vestido_exemplo.png',
     nome:'Vestido social de exemplo',
     info:'Vestido social embedado em joias para eventos sociais',
-    peso:'200g',tamanho:'M',material:'algodão',
+    peso:'679g',tamanho:'M',material:'nanofibras e zircônio',
     valorAntigo:'',
-    valorAtual:'R$ 110,89',
+    valorAtual:'R$ 378,89',
     categoria:['Vestido']}
 
 ]//||------||------|| FIM DOS PRODUTOS DA LOJA ||------||------||
 
 function startIndice(){// ATRIBUI O INDICE DE CADA ITEM AUTOMATICAMENTE
     for(c in produtosLoja){
-        produtosLoja[c].indice=Number(c)+1}
+        produtosLoja[c].indice=Number(c)}
 }startIndice()
 
 function cleanProduct(){// REMOVE TODOS OS PRODUTOS DO SITE PARA ATUALIZAÇÃO
@@ -95,10 +95,25 @@ function loadProduct(filtroSelecionado){// CARREGA E FILTRA PRODUTOS SE NECESSÁ
 }loadProduct("none");
 
 function showProduct(showIs){// APRESENTA PRODUTO SELECIONADO NO MOSTRUÁRIO
-    if(showIs === 0){
+    if(showIs === -1){
         window.document.getElementById('showProduct').style="display:none;"
+        window.document.querySelector("#mainSlider").style="display: block;"
     }else{
-        window.document.getElementById('showProduct').style="display:flex;"
-        window.document.getElementById('showProduct').innerHTML+=`${showIs}`
+        window.document.querySelector("#mainSlider").style="display: none;"
+        //Rola a tela até o topo
+        window.scrollTo(0,0)
+        //Habilita visualização do mostruário
+        window.document.getElementById('showProduct').style="display:grid;"
+        //Atualiza NOME do produto selecionado
+        window.document.querySelector(".pshowName").innerHTML=produtosLoja[showIs].nome
+        //Atualiza PESO, TAMANHO, MATERIAL e INFORMAÇÃO
+        window.document.querySelector(".pshowDef").innerHTML="<strong>Peso:</strong> "+produtosLoja[showIs].peso+"<br><strong>Tamanho:</strong> "+produtosLoja[showIs].tamanho+"<br><strong>Material:</strong> "+produtosLoja[showIs].material+"<br><br><hr><strong>Descrição do produto<br></strong>"+produtosLoja[showIs].info
+        //Atualiza IMAGEM PRINCIPAL produto selecionado
+        window.document.querySelector(".pshowMainimage").setAttribute('src',produtosLoja[showIs].img)
+        //Atualiza IMAGENS SEGUNDARIAS prod.selecionado
+        window.document.querySelector(".SecimgA").setAttribute('src',produtosLoja[showIs].img)
+        window.document.querySelector(".SecimgB").setAttribute('src',produtosLoja[showIs].img)
+        window.document.querySelector(".SecimgC").setAttribute('src',produtosLoja[showIs].img)
+        
     }
 }
