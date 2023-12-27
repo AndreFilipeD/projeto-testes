32f32ba322ebdf2ba41c08a222e591e3bd2c11e6
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-12-27 11:03:41 -0300

    Treinando getter e setter

--- html-css-javascript/oriobject/script.js
+++ html-css-javascript/oriobject/script.js
@@ -1,21 +1,30 @@
 let pessoas = []
 
 class person{
-    constructor(nome, idade){
+    age = 10
+    steps = 0
+
+    constructor(nome){
         this.name = nome
-        this.age = idade
     }
+
+    takeAstep(){
+        this.steps++
+    }
+    
 }
 
 function create(){
-    pessoas.push(new person(window.document.querySelector('#texti').value, pessoas.length))
-    send()
+    pessoas.push(new person(window.document.querySelector('#texti').value));
+    send();
 }
 
 function send(){
+    pessoas[0].takeAstep(); //aumenta steps 1 pessoa
+
     window.document.querySelector("#bod").innerHTML=''
     for(c in pessoas){
-        window.document.querySelector("#bod").innerHTML+='<h1 class="button">' + pessoas[c].name + ' [' +pessoas[c].age + '] ' + '</h1><hr>'
+        window.document.querySelector("#bod").innerHTML+='<h1 class="button">' + pessoas[c].name + ' [' +pessoas[c].steps + '] ' + 'age: '+pessoas[c].age+' ||</h1><hr>';
     }
 }
 
