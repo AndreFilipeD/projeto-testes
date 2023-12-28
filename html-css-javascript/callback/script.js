35fe556b8b4568bd470d41809ce0cf716c561128
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-12-28 11:56:57 -0300

    Criado primeiro projeto Callback funcional

---
+++ html-css-javascript/callback/script.js
@@ -0,0 +1,22 @@
+// https://jsonplaceholder.typicode.com/posts
+let cont = 0;
+window.document.querySelector("#update").addEventListener('click',()=>{
+    updateContent()
+})
+
+function updateContent(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response)=>{
+            return response.json()
+        }
+    ).then((data)=>{
+        cont = 0
+        window.document.querySelector("#space").innerHTML=""
+        for(c in data){
+            window.document.querySelector("#space").innerHTML+=`<div class="element">Titulo: ${data[c].title}|ID:${data[c].id}|USERID:${data[c].userId}|BODY:${data[c].body}</div>`
+            cont++
+        }window.document.querySelector("#update").innerHTML=`Encontrado ${cont} Arquivos.`
+
+        console.log(data[0])
+    })
+}
