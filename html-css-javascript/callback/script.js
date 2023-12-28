75e43f449c0c97bceda1e4c0c0cf35bf1118e338
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-12-28 20:00:32 -0300

    Ultima atualização do dia

--- html-css-javascript/callback/script.js
+++ html-css-javascript/callback/script.js
@@ -5,22 +5,20 @@ window.document.querySelector("#update").addEventListener('click',()=>{
 })
 
 function updateContent(){
-    fetch('https://jsonplaceholder.typicode.com/po2sts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response)=>{
             return response.json()
-        })
+    })
     .then((data)=>{
         cont = 0
         window.document.querySelector("#space").innerHTML=""
         for(c in data){
             window.document.querySelector("#space").innerHTML+=`<div class="element">Titulo: ${data[c].title}|ID:${data[c].id}|USERID:${data[c].userId}|BODY:${data[c].body}</div>`
             cont++
-        }window.document.querySelector("#update").innerHTML=`Encontrado ${cont} Arquivos.`
-    })
-    .catch(()=>{
-        window.alert('Algo de errado não esta certo')
-    })
-    .finally(()=>{
-        window.alert('Fim do processo.')
+        }window.document.querySelector("#update").innerHTML=`Encontrado ${cont} Arquivos.`;
+    }).catch(()=>{
+        window.alert('Algo de errado não esta certo');
+    }).finally(()=>{
+        window.alert('Fim do processo.');
     })
 }
