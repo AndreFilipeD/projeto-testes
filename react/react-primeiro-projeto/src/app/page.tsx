7818eeed15c088623d5c09c8d32b454da6a36a68
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2024-01-10 11:16:27 -0300

    Import basico em REACT funcionando

--- react/react-primeiro-projeto/src/app/page.tsx
+++ react/react-primeiro-projeto/src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Square , Circle} from '@/components/Geo';
+
 const Page = () => {
   return(
     <div>
       <h1>Olá, Mundo!</h1>
       <h3>Finalmente consegui fazer algo com REACT</h3>
+      <Square />
+      <br />
+      <Circle />
     </div>
   );
 }
---
+++ react/react-primeiro-projeto/src/components/Geo.tsx
@@ -0,0 +1,15 @@
+export const Square = () => {
+    return(
+        <div className="w-50 h-50 bg-orange-700 text-white">
+            Quadradin
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return(
+        <div className="w-50 h-50 bg-orange-700 text-white rounded-full">
+            Circulin
+        </div>
+    )
+}
