9e0f97e82feddffec6bed061a307c4adab68d0a6
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2024-02-26 10:14:30 -0300

    Praticando conhecimentos em React

--- react-testes/reactestes/src/app/page.tsx
+++ react-testes/reactestes/src/app/page.tsx
@@ -1,5 +1,15 @@
+
+import "./style.css"
+
+
 export default function Home() {
-  return(
-    <></>
+
+  return (
+    <>
+      <div className="main">
+      <p className="one">It's Really works</p>
+      <p>Congratulations</p>
+      </div>
+    </>
   )
-  }
+}
---
+++ react-testes/reactestes/src/app/usuarios.tsx
@@ -0,0 +1,59 @@
+type Props = {
+    id: number;
+    name: string;
+    email: string;
+    avatar: string;
+    active: boolean;
+    nota1: number;
+    nota2: number;
+}
+
+export const users = [
+    {
+    id: 1,
+    name: 'carlos',
+    email: 'carlos@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    active: false,
+    nota1: 7.3,
+    nota2: 8.1
+    }
+    ,
+    {
+    id: 2,
+    name: 'amanda',
+    email: 'amanda@escola.com.br',
+    avatar:'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    active: true,
+    nota1: 9.5,
+    nota2: 9.2
+    }
+    ,
+    {
+    id: 1,
+    name: 'julia',
+    email: 'julia@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    active: false,
+    nota1: 4.3,
+    nota2: 2.4
+    }
+    ,
+    {
+    id: 1,
+    name: 'bruno',
+    email: 'bruno@escola.com.br',
+    avatar: 'https://images.unsplash.com/photo-1463453091185-61582044d556?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    active: true,
+    nota1: 5.6,
+    nota2: 7.7
+    }
+]
+
+export const User = () => {
+    return(
+        <>
+            
+        </>
+    )
+}
