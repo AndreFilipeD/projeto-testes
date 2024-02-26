c043cf0b68b75139eabf4a3518f82d9796ed56ae
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2024-02-26 10:39:32 -0300

    Praticando conceitos basicos do React

--- react-testes/reactestes/src/app/page.tsx
+++ react-testes/reactestes/src/app/page.tsx
@@ -2,12 +2,12 @@
 import "./style.css"
 
 
-export default function Home() {
+export default function Bring() {
 
   return (
     <>
       <div className="main">
-      <p className="one">It's Really works</p>
+      <p className="one">FileNameBased to test!</p>
       <p>Congratulations</p>
       </div>
     </>
